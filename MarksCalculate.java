8d00022a5467d6d123bbd3b516085dcf910d28c9
sewar shorman <158173670+Sewar77@users.noreply.github.com>
2024-07-11 00:24:39 +0300

    Update MarksCalculate.java

--- MarksCalculate.java
+++ MarksCalculate.java
@@ -1,9 +1,7 @@
-
 import java.util.Scanner;
 public class MarksCalculate {
-
 	public static void main(String[] args) {
-		//suppose there is 7 subjects. 
+		//suppose there are 7 subjects. 
 		Scanner read = new Scanner(System.in);
 		float marks = 0;
 		int counter = 0;
@@ -14,33 +12,29 @@ public class MarksCalculate {
 				marks += mark;
 				counter++;
 			}
-		}
+			else{
+				System.out.prinln("Wrong mark input");
+			}
+			
 		System.out.println("Total marks: " + (marks));
 		float avrage = marks/counter;
 		System.out.println("Average marks: " + avrage);
 		System.out.println("marks in perc: " + (marks/counter) + "% ");
 			if (avrage<50)
-				System.out.println("F");
+				System.out.println("Your final grade is F");
 			else if (avrage >= 50 && avrage < 60)
-				System.out.println("E");
+				System.out.println("Your final grade is E");
 			else if (avrage >= 60 && avrage < 70)
-				System.out.println("D");
-			
+				System.out.println("Your final grade is D");
 			else if (avrage >= 70 && avrage < 80)
-				System.out.println("C ");
-		
+				System.out.println("Your final grade is C ");
 			else if (avrage >= 80 && avrage < 90)
-				System.out.println("B");
+				System.out.println("Your final grade is B");
 			
 			else if(avrage >= 90 && avrage <= 100)
-				System.out.println("A");
+				System.out.println("Your final grade is A");
 			else {
 				System.out.println("invalid input");
 			}
-		}
-		
-		
-		
-		
+		}	
 	}
-
